90c7e5e97fb257ed226ae5835ef7b7036254df81
김덕수 <deocksoo@gimdeogsus-MacBook-Pro.local>
2022-07-23 14:30:25 +0900

    initial commit

---
+++ src/main/java/co/deering/learningiotrelay/LearningIotRelayApplication.java
@@ -0,0 +1,13 @@
+package co.deering.learningiotrelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningIotRelayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningIotRelayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/deering/learningiotrelay/LearningIotRelayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.deering.learningiotrelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningIotRelayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
